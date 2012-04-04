1fe9e6a8bdc1329014bb817138d01654194e1c91
Jincux <dephurites@gmail.com>
2012-04-03 22:02:31 -0400

    Implemented core

---
+++ core.cs
@@ -0,0 +1,36 @@
+new ScriptGroup(BLG) {
+	internalVersion = "1.0.A1";
+	externalVersion = "1.0 Alpha 1";
+
+	implementation = ""; //server or client
+	loaded = false;
+
+	debugLevel = 1;
+	//0 = No Debug
+	//1 = Standard
+	//2 = In-depth
+	//3 = Spam me. Please.
+};
+
+function BLG::initiatorLoaded(%this) {
+	if(%this.implementation $= "server") {
+		echo("Loading BLG [" @ %this.internalVersion @ "] server implementation");
+	} else if(%this.implementation $= "client") {
+		echo("Loading BLG [" @ %this.internalVersion @ "] client implementation");
+	} else {
+		%this.debug("Unresolved Initiator");
+		error("Failed to load BLG [" @ %this.internalVersion @ "]. Please redownload from blocklandglass.com");
+	} 
+}
+
+function BLG::debug(%this, %msg, %level) {
+	if(%level $= "") {
+		%level = 1;
+	}
+
+	if(%level > %this.debugLevel) {
+		return;
+	}
+
+	echo("\c5BLG Debug >>\c1 " @ %msg);
+}
---
+++ server.cs
@@ -0,0 +1,4 @@
+//Blockland Glass Version 1.0 Server Implementation
+exec("./core.cs");
+BLG.implementation = "server";
+BLG.initiatorLoaded();
