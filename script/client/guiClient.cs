24c5e4af0ff288d51e9d77ff5083754abc86ca5c
Jincux <dephurites@gmail.com>
2012-04-04 17:37:35 -0400

    Added Callback support, protection against bad code, and disconnect cleanup

--- script/client/guiClient.cs
+++ script/client/guiClient.cs
@@ -3,7 +3,7 @@
 //================================================
 
 if(!isObject(BLG_GC)) new ScriptObject(BLG_GC); //Main Object
-if(!isObject(BLG_GCSG)) new SimGroup(BLG_GCSG); //Clean-up group for disconnect
+if(!isObject(BLG_GCSG)) new ScriptGroup(BLG_GCSG); //Clean-up group for disconnect
 BLG_GC.SG = BLG_GCSG;
 
 //Protocol:
@@ -26,7 +26,7 @@ function BLG_GC::verifyString(%this, %string) { //Checks sent message to make su
 }
 
 function BLG_GC::finalizeObject(%this, %objId) {
-	%obj = %this.tempObjData[%objId];
+	%obj = %this.SG.objData[%objId];
 	if(isObject(%obj)) {
 		%eval = "%obj = new " @ %obj.objClass @ "(" @ %obj.name @ "){";
 
@@ -35,6 +35,9 @@ function BLG_GC::finalizeObject(%this, %objId) {
 		}
 
 		%eval = %eval @ "BLG__OBJID=" @ %objId @ ";";
+		if(%obj.command !$= "") {
+			%eval = %eval @ "command=" @ %obj.command @ ";";
+		}
 		%eval = %eval @ "};$BLG::GC::TempObj = %obj;";
 		BLG.debug("Executing Object [" @ %objId @ "]...");
 		BLG.debug(%eval, 3);
@@ -58,15 +61,15 @@ function BLG_GC::finalizeObject(%this, %objId) {
 }
 
 function BLG_GC::setObjectAttribute(%this, %objId, %data, %value) {
-	%obj = %this.tempObjData[%objId];
+	%obj = %this.SG.objData[%objId];
 	%obj.attributeDat[%obj.attributes] = %data; 
 	%obj.attributeVal[%obj.attributes] = %value;
 	%obj.attributes++;
 }
 
 function BLG_GC::setObjectChild(%this, %objId, %subId) {
-	%obj = %this.tempObjData[%objId];
-	%child = %this.tempObjData[%subId];
+	%obj = %this.SG.objData[%objId];
+	%child = %this.SG.objData[%subId];
 	if(%child.parent $= "") {
 		%obj.child[%obj.children] = %child;
 		%child.parent = %obj;
@@ -88,7 +91,7 @@ function BLG_GC::initiateObject(%this, %objId, %objClass, %name, %root) {
 		attributes = 0;
 	};
 
-	%this.tempObjData[%objId] = %obj;
+	%this.SG.objData[%objId] = %obj;
 	%this.SG.add(%obj);
 }
 
@@ -98,17 +101,23 @@ function BLG_GC::handleMessage(%this, %msg) {
 
 	switch(%funcId) {
 		case 0: //New Object
-			if(BLG_GC.tempObjData[%objId] !$= "") {
+			if(BLG_GCSG.objData[%objId] !$= "") {
 				//Object already exists, someone broke the server mod
 				BLG.debug("GUI Object sent for creation with objId [" @ %objId @ "] is using a already existing objId", 0);
 			} else {
 				%objClass = getField(%msg, 2);
 				%name = getField(%msg, 3);
 				%root = getField(%msg, 4);
+
+				if(!%this.verifyString(%objClass) || !%this.verifyString(%name)) {
+					//Watch out guys, we're dealing with a badass over here
+					BLG.debug("Verify String test returned false for objId [" @ %objId @ "]. Could be possible attempt to run malicious script");
+				}
+
 				if(%objClass $= "") {
-					BLG.debug("Invalid initiate message [" @ %msg @ "] (bad class)");
+					BLG.debug("Invalid initiate message [" @ %msg @ "] (bad class)", 0);
 				} else if(%root != 1 && %root != 0 && %root !$= "") {
-					BLG.debug("Invalid initiate message [" @ %msg @ "] (bad root value)");
+					BLG.debug("Invalid initiate message [" @ %msg @ "] (bad root value)", 0);
 				}
 
 				if(%root) {
@@ -118,7 +127,7 @@ function BLG_GC::handleMessage(%this, %msg) {
 			}
 
 		case 1: //Set Attribute
-			if(BLG_GC.tempObjData[%objId] $= "") {
+			if(BLG_GCSG.objData[%objId] $= "") {
 				//Object already exists, someone broke the server mod
 				BLG.debug("GUI Object attribute change for objId [" @ %objId @ "] is trying to change a non-existant object!", 0);
 			} else {
@@ -128,13 +137,16 @@ function BLG_GC::handleMessage(%this, %msg) {
 				if(%data $= "") {
 					BLG.debug("GUI Object attribute change for objId [" @ %objId @ "] is trying to change a blank attribute!", 0);
 				}
+				if(%value $= "command") {
+					BLG.debug("GUI Object id [" @ %objId @ "] tried to register a callback invalidly. Possible attemp to run bad code.", 0);
+				}
 
 				%this.setObjectAttribute(%objId, %data, %value);
 			}
 
 		case 2: //Set Parent Object (After 0-1 are finished)
 			%parentId = getField(%msg, 2);
-			if(BLG_GC.tempObjData[%objId] $= "" || BLG_GC.tempObjData[%parentId] $= "") {
+			if(BLG_GCSG.objData[%objId] $= "" || BLG_GCSG.objData[%parentId] $= "") {
 				BLG.debug("GUI Object id [" @ %objId @ "] and/or [" @ %parentId @ "] is invalid, but tried to parent");
 			} else {
 				%this.setObjectChild(%objId, %parentId);
@@ -145,5 +157,23 @@ function BLG_GC::handleMessage(%this, %msg) {
 				%this.finalizeObject(getWord(%this.roots, %i));
 			}
 
+		//All from here are non-generic calls
+
+		case 4: //Set custom callback
+			%command = "\"BLG_GC.handleCallback(" @ %objId @ ");\"";
+			%obj = BLG_GCSG.objData[%objId];
+			%obj.command = %command;
 	}
 }
+
+package BLG_GC_Package {
+	function disconnect() {
+		parent::disconnect();
+		for(%i = 0; %i < BLG_GCSG.getCount(); %i++) {
+			%obj = BLG_GCSG.getObject(%i);
+			canvas.popDialog(%obj);
+		}
+		BLG_GCSG.deleteAll();
+	}
+};
+activatePackage(BLG_GC_Package);
