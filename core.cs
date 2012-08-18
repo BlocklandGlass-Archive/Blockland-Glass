99d4a686221a075b690b8c1fe700b652539beb39
Jincux <jincux@gmail.com>
2012-08-18 18:31:26 -0400

    1.3

--- core.cs
+++ core.cs
@@ -1,6 +1,6 @@
 new ScriptGroup(BLG) {
-	internalVersion = "1.2.1";
-	externalVersion = "1.2.1";
+	internalVersion = "1.3";
+	externalVersion = "1.3";
 	versionId = 3; //1 is anything before 1.2
 
 	debugLevel = 3;
